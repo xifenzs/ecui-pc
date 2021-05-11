bfdf41a8309b2c59753f76bbce1d407b8d711a57
李万才 <liwancai@yiche.com>
2021-05-11 17:26:03 +0800

    style: 优化

--- _include/filter.js
+++ _include/filter.js
@@ -1,30 +1,30 @@
-(function() {
+(function () {
     /*定义etpl过滤器
      *
      *addFilter参数
      *{string}name - 过滤器名称
      *{Function}filter - 过滤函数
      */
-    etpl.addFilter('stringify', function(value) {
+    etpl.addFilter('stringify', function (value) {
         return JSON.stringify(value || {});
     });
-    etpl.addFilter('toFixed', function(value, divisor, fixedNum) {
+    etpl.addFilter('toFixed', function (value, divisor, fixedNum) {
         return (Number(value) / divisor).toFixed(fixedNum);
     });
     // 根据 code 从 baseInfoMap 中解析数据
-    etpl.addFilter('parseBaseInfo', function(value, nameSpace) {
+    etpl.addFilter('parseBaseInfo', function (value, nameSpace) {
         return yiche.info.baseInfoMap[nameSpace][value.toString()] || '--';
     });
     // 时间转换过滤器
-    etpl.addFilter('dateFormat', function(value, format) {
-        return value ? util.formatDate(new Date(Number(value)), format) : '';
+    etpl.addFilter('dateFormat', function (value, format) {
+        return value ? yiche.util.formatDate(new Date(Number(value)), format) : '';
     });
     //数据为空时用 --代替
-    etpl.addFilter('default', function(value) {
+    etpl.addFilter('default', function (value) {
         return value || '--';
     });
     // 数字 保留两位小数，加千分位
-    etpl.addFilter('numberFormat', function(num) {
+    etpl.addFilter('numberFormat', function (num) {
         if (num === '' || num.length < 1 || num === '--') {
             return '--';
         }
@@ -36,12 +36,11 @@
         }
         return num;
     });
-    etpl.addFilter('parseBr', function(value) {
-        var value = value.replace(/\n|\\n/g, '<br/>');
-        return value || '--';;
+    etpl.addFilter('parseBr', function (value) {
+        return value.replace(/\n|\\n/g, '<br/>') || '--';
     });
 
-    etpl.addFilter('toThousands', function(value) {
+    etpl.addFilter('toThousands', function (value) {
         return yiche.util.getToThousands(value);
     });
     /* 自定义etpl过滤器 - end */
