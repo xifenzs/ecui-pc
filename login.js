91017a8fa2d27225bcbd2d9d4bde0e938351b93b
李万才 <liwancai@yiche.com>
2021-05-13 11:30:18 +0800

    style: 样式优化

--- login.js
+++ login.js
@@ -3,10 +3,6 @@
         var intervalId = setInterval(function () {
             if (ecui.getNamedControls()['login-btn']) {
                 clearInterval(intervalId);
-                var sUuid = 0;
-                //获取验证码
-                // var getActive = ecui.get('send-captcha').getOuter();
-                var getActiveClick = true;
                 var error = ecui.$('error');
                 var showError = function (msg) {
                     error.innerHTML = msg;
@@ -16,28 +12,12 @@
                     error.innerHTML = '';
                     ecui.dom.addClass(error, 'ui-hide');
                 };
-                var countDown = function () {
-                    var time = 60;
-                    getActive.style.color = '#cccccc';
-                    var timer = setInterval(clock, 1000);
-                    clock();
-                    function clock() {
-                        time--;
-                        getActive.value = time + '秒后重发';
-                        if (time === 0) {
-                            clearInterval(timer);
-                            getActive.value = '重获验证码';
-                            getActive.style.color = '#368cda';
-                            getActiveClick = true;
-                        }
-                    }
-                };
+              
                 var login = function () {
                     var data = {
                         userName: encodeURIComponent(ecui.get('mobile').getValue()),
                         password: ecui.get('password').getValue()
                     };
-                    // 'userName=' + encodeURIComponent(ecui.get('mobile').getValue()) + '&password=' + ecui.get('password').getBody().value
                     ecui.io.ajax('/serve-user/login', {
                         method: 'post',
                         data: JSON.stringify(data),
@@ -67,35 +47,6 @@
                         login();
                     }
                 };
-                // ecui.get('send-captcha').onclick = function () {
-                //     if (getActiveClick) {
-                //         getActiveClick = false;
-                //         ecui.io.ajax('send-sms-captcha', {
-                //             method: 'post',
-                //             data: 'mobile=' + encodeURIComponent(ecui.get('mobile').getValue()),
-                //             headers: {
-                //                 'Content-Type': 'application/x-www-form-urlencoded'
-                //             },
-                //             onsuccess: function (data) {
-                //                 data = JSON.parse(data);
-                //                 if (data.code === 0) {
-                //                     hideError();
-                //                     sUuid = data.data.uuid;
-                //                     // countDown();
-                //                     yiche.showHint('success', '发送成功');
-                //                 } else {
-                //                     showError(data.msg);
-                //                     yiche.showHint('warn', data.msg);
-                //                     getActiveClick = true;
-                //                 }
-                //             },
-                //             onerror: function (code, msg) {
-                //                 showError(msg);
-                //                 yiche.showHint('warn', msg);
-                //             }
-                //         });
-                //     }
-                // };
                 ecui.get('login-btn').onclick = function () {
                     login();
                 };
